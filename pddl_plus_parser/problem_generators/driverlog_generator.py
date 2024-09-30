fe7f83f6ba3923cedafebe61f84662672488e3d4
Argaman Mordoch <argaman.aloni@gmail.com>
2024-09-30 15:58:02 +0300

    added the discrete version of driverlog to the PDDL generators. next should make the creation process more general.

--- pddl_plus_parser/problem_generators/driverlog_generator.py
+++ pddl_plus_parser/problem_generators/driverlog_generator.py
@@ -4,12 +4,12 @@ import sys
 from pathlib import Path
 
 
-def generate_problems(output_directory: Path, num_probs_per_difficulty: int = 100):
+def generate_numeric_problems(output_directory: Path, num_probs_per_difficulty: int = 100):
     # The following code is a part of the driverlog_generator.py file
     # It is used to generate problems for the driverlog domain
     # The code is used to generate problems for learning purposes
     # It is not used in the main application
-    print("generating problems for the driverlog domain...")
+    print("generating numeric problems for the driverlog domain...")
     for i in range(num_probs_per_difficulty):
         # Generate problems for the easy difficulty
         problem_name = f"pfile{i}.pddl"
@@ -27,9 +27,27 @@ def generate_problems(output_directory: Path, num_probs_per_difficulty: int = 10
         subprocess.check_output(generate_problem_command, shell=True)
 
 
+def generate_strips_problems(output_directory: Path, max_num_trucks: int = 8, max_num_packages: int = 50,
+                             max_drivers: int = 8):
+    # The following code is a part of the driverlog_generator.py file
+    # It is used to generate problems with multiple drivers in strips format.
+    print("generating propositional problems for the driverlog domain...")
+    for i in range(100):
+        # Generate problems for the easy difficulty
+        problem_name = f"pfile{i}.pddl"
+        problem_path = output_directory / problem_name
+        generate_problem_command = f"./dlgen {random.randint(1, 100)} {random.randint(3, 10)} {random.randint(3, max_drivers)} {random.randint(2, max_num_packages)} {random.randint(2, max_num_trucks)} 100 > {problem_path}"
+        print(f"generating problem {problem_name}...")
+        subprocess.check_output(generate_problem_command, shell=True)
+
+
 def main():
     output_directory = sys.argv[1]
-    generate_problems(Path(output_directory))
+    if sys.argv[2] == "numeric":
+        generate_numeric_problems(Path(output_directory))
+
+    elif sys.argv[2] == "strips":
+        generate_strips_problems(Path(output_directory))
 
 
 if __name__ == "__main__":
